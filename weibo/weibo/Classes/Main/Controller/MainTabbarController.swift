52070054e0fc74ec5c59c8122ad17fbe21f72e61
GuitarPlayer-Ma <138910152@qq.com>
2015-09-27 10:11:11 +0800

    初始化项目: 1.初始化项目的目录结构,MVC设计模式 2.添加启动图片和图标

---
+++ weibo/weibo/Classes/Main/Controller/MainTabbarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabbarController.swift
+//  weibo
+//
+//  Created by mada on 15/9/27.
+//  Copyright © 2015年 MD. All rights reserved.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+    
+}
